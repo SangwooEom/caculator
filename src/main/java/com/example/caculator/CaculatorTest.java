fc74618737a7f8a790a9386c5fc93bdeab9d4e07
sangwooeom <cdm19720@gamil.com>
2023-07-22 15:04:27 +0900

    Spring Test 라이브러리 추가

--- src/main/java/com/example/caculator/CaculatorTest.java
+++ src/main/java/com/example/caculator/CaculatorTest.java
@@ -2,7 +2,7 @@ package com.example.caculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class CaculatorTest {
 	private Caculator caculator = new Caculator();
